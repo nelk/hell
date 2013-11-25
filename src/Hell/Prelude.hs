4642095b62e4b912490d65ab1f6724ac5b6fa15a
nelk <nelkishere@gmail.com>
2013-11-25 11:07:29 -0500

    Made cd change directory in main process and not child process.

--- src/Hell/Prelude.hs
+++ src/Hell/Prelude.hs
@@ -4,13 +4,10 @@
 module Hell.Prelude where
 
 import Prelude
-import Control.Monad
-import Data.List
 import System.Directory
 import System.Exit
 import System.IO
 #ifdef USE_OLD_TIME
-import System.Time
 #else
 import Data.Time.Clock
 #endif
@@ -18,122 +15,29 @@ import System.Process
 import Data.Maybe
 import Data.Typeable
 import Control.Applicative
-import Data.String.Utils (split)
+import Text.Regex.Posix
+
+-- TODO - Remove "/bin/sh: 1: " from error messages; have a way to give ghc error (prefix?); prevent ctrl+c from leaving.
 
 -- | setCurrentDirectory
 cd :: FilePath -> IO ()
 cd = setCurrentDirectory
 
--- | getCurrentDirectory
-pwd :: IO FilePath
-pwd = getCurrentDirectory
-
--- | getHomeDirectory
-home :: IO FilePath
-home = getHomeDirectory
-
--- | getTemporaryDirectory
-tmp :: IO FilePath
-tmp = getTemporaryDirectory
-
--- | removeFile
-rm :: FilePath -> IO ()
-rm = removeFile
-
--- | renameFile
-mv :: FilePath -> FilePath -> IO ()
-mv = renameFile
-
--- | renameDirectory
-mvdir :: FilePath -> FilePath -> IO ()
-mvdir = renameDirectory
-
--- | copyFile
-cp :: FilePath -> FilePath -> IO ()
-cp = copyFile
-
--- | findExecutable
-whereis :: String -> IO (Maybe FilePath)
-whereis = findExecutable
-
--- | getPermissions
-perms :: FilePath -> IO Permissions
-perms = getPermissions
-
--- | getModificationTime
-#ifdef USE_OLD_TIME
-modified :: FilePath -> IO ClockTime
-#else
-modified :: FilePath -> IO UTCTime
-#endif
-modified = getModificationTime
-
--- | removeDirectory
-rmdir :: FilePath -> IO ()
-rmdir = removeDirectory
-
--- | removeDirectoryRecursive
-rmdirR :: FilePath -> IO ()
-rmdirR = removeDirectoryRecursive
-
--- | createDirectory
-mkdir :: FilePath -> IO ()
-mkdir = createDirectory
-
--- | createDirectoryIfMissing
-mkdirF :: Bool -> FilePath -> IO ()
-mkdirF = createDirectoryIfMissing
-
--- | dir' >=> mapM_ putStrLn
-dir :: FilePath -> IO ()
-dir = dir' >=> mapM_ putStrLn
-
--- | fmap (sort . filter (not . isPrefixOf \".\")) (getDirectoryContents d)
-dir' :: FilePath -> IO [[Char]]
-dir' d = fmap (sort . filter (not . isPrefixOf ".")) (getDirectoryContents d)
-
--- | ls' >>= mapM_ putStrLn
-ls :: IO ()
-ls = ls' >>= mapM_ putStrLn
-
--- | dir' \".\"
-ls' :: IO [[Char]]
-ls' = dir' "."
-
--- | fmap sort (getDirectoryContents \".\")
-lsa :: IO [FilePath]
-lsa = fmap sort (getDirectoryContents ".")
-
--- | readFile
-cat :: FilePath -> IO String
-cat = readFile
-
--- | writeFile
-write :: FilePath -> String -> IO ()
-write = writeFile
-
--- | appendFile
-append :: FilePath -> String -> IO ()
-append = appendFile
-
--- | hFileSize
-size :: FilePath -> IO Integer
-size fp = do
-  h <- openFile fp ReadMode
-  s <- hFileSize h
-  hClose h
-  return s
-
--- | putStrLn
-echo :: String -> IO ()
-echo = putStrLn
-
 -- | system cmd
 run' :: String -> IO ExitCode
 run' cmd = system cmd
 
+-- Returns Just with IO to run if there was a cd command, and Nothing if it should be run normally.
+runWithExtractedCd :: String -> Maybe (IO String)
+runWithExtractedCd cmd = let (_, _, _, groups) = cmd =~ "^ *cd ([^ \"]+)" :: (String, String, String, [String])
+                         in if length groups == 1
+                            then Just $ cd (head groups) >> return ""
+                            else Nothing
+-- TODO - Capture anywhere using "^[^\"]*(\"[^\"]\")*[\"]*cd ([^ \"]+)"
+
 -- Run with stdin input
 run_ :: String -> IO String
+run_ cmd | isJust (runWithExtractedCd cmd) = fromJust $ runWithExtractedCd cmd
 run_ cmd = do out <- pipeThroughCmd cmd (Right $ Inherit) Inherit
               case out of
                 Left s -> return s
@@ -142,6 +46,7 @@ run_ cmd = do out <- pipeThroughCmd cmd (Right $ Inherit) Inherit
 
 -- Run multiple piped commands (with string input into first)
 run :: String -> String -> IO String
+run cmd _ | isJust (runWithExtractedCd cmd) = fromJust $ runWithExtractedCd cmd
 run cmd input = do out <- pipeThroughCmd cmd (Left $ input) Inherit
                    case out of
                     Left s -> return s
@@ -150,8 +55,8 @@ run cmd input = do out <- pipeThroughCmd cmd (Left $ input) Inherit
 
 pipeThroughCmd :: String -> Either String StdStream -> StdStream -> IO (Either String StdStream)
 pipeThroughCmd cmd pipe_in pipe_out =
-                   do d <- pwd
-                      (stdin_m, stdout_m, stderr_m, p) <- createProcess $
+                   do d <- getCurrentDirectory
+                      (stdin_m, stdout_m, _, p) <- createProcess $
                         CreateProcess
                           { cmdspec = ShellCommand cmd
                           , cwd = Just d
